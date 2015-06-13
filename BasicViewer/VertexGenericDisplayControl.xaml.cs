e453f4972f72dae8201e944cddc68a473cd87925
csnemes <csaba.nemes@outlook.com>
2015-06-13 11:18:40 +0200

    nav display

--- BasicViewer/VertexGenericDisplayControl.xaml.cs
+++ BasicViewer/VertexGenericDisplayControl.xaml.cs
@@ -31,10 +31,59 @@ namespace BasicViewer
             tree.Items.Clear();
             foreach (var vertex in vertices)
             {
-                var item = new TreeViewItem();
-                item.Header = vertex.Id;
-                tree.Items.Add(item);
+                tree.Items.Add(CreateVertexNode(vertex));
             }
+        }
+
+        private void TreeViewItemExpanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvi = e.OriginalSource as TreeViewItem;
+            if (tvi != null)
+            {
+                if (tvi.Items != null && tvi.Items.Count > 0) return; //already filled
+
+                //ket nodefajta lehet vertex vagy a navigációs cucc
+                if (tvi.Tag.GetType() == typeof(Vertex))
+                {
+                    var vertex = (Vertex)tvi.Tag;
+                    var navs = vertex.GetNavigationPossibilities();
+                    foreach (var nav in navs)
+                    {
+                        tvi.Items.Add(CreateLinkNode(nav));
+                    }
+                }
+                else
+                {
+                    var navType = (string)tvi.Tag;
+                    var parentVertex = (Vertex)((TreeViewItem)tvi.Parent).Tag;
+                    var childs = parentVertex.NavigateOn(navType);
+
+                    foreach (var vertex in childs)
+                    {
+                        tvi.Items.Add(CreateVertexNode(vertex));
+                    }
+                }
+
+            }
+        }
+
+        private TreeViewItem CreateLinkNode(string linkType)
+        {
+            var newItem = new TreeViewItem();
+            newItem.Header = linkType;
+            newItem.Tag = linkType;
+            newItem.Expanded += new RoutedEventHandler(TreeViewItemExpanded);
+            return newItem;
+        }
+
+        private TreeViewItem CreateVertexNode(Vertex vertex)
+        {
+            var item = new TreeViewItem();
+            item.Header = vertex.Id;
+            item.Expanded += new RoutedEventHandler(TreeViewItemExpanded);
+            item.Tag = vertex;
+            return item;
         }
     }
 }
+
